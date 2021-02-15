1568e4bbc9bf19f74db72435d119bd1ceaf01a24
jianweibi <77766857+jianweibi@users.noreply.github.com>
2021-02-15 12:32:56 -0800

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+let name = "Jianwei Bi";
+const msg = `Welcome to visit my homepage! Here you will find my basic info.`;
+function printPerson(name, msg){
+    console.log(`Hello, this is ${name}.`);
+    console.log(msg);
+}
+printPerson(name, msg);
