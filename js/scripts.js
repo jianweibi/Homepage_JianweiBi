91753e14bd673a4e7c7bf39c359382f87c700a7c
bekezie <73091370+bekezie@users.noreply.github.com>
2021-02-17 07:49:15 -0800

    Great that you were able to include some JavaScript into your project!
    Great that you were able to include some JavaScript into your project! I would suggest that in the future creating JavaScript code that creates change
    to your website which your viewers can see rather than only in the console.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+Great that you were able to include some JavaScript into your project! I would suggest that in the future creating JavaScript code that creates change
+to your website which your viewers can see rather than only in the console.
+
 let name = "Jianwei Bi";
 const msg = "Welcome to visit my homepage! Here you will find my basic info.";
 function printPerson(name, msg){
